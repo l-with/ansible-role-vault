da4ca1efb51d10d91013aa1b18cb5436d136e37b
Lars With <lars@with.de>
2021-07-01 13:56:04 +0200

    rm /srv/vault/* at all

--- try/cleanup.sh
+++ try/cleanup.sh
@@ -3,8 +3,4 @@
 docker kill vault
 docker rm vault
 
-sudo rm -rvf /srv/vault/file/raft/*
-sudo rm -rvf /srv/vault/config/.vault_*
-sudo rm -rvf /srv/vault/policies/*
-sudo rm -rvf /srv/vault/config/policies/*
-sudo rm -v /srv/vault/ansible_done*
+sudo rm -rvf /srv/vault/*
