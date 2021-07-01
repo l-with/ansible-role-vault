0c3e7b62bab166b2e0695bc6e0ddb74e64899439
Lars With <lars@with.de>
2021-07-01 11:51:07 +0200

    add rm files

--- try/cleanup.sh
+++ try/cleanup.sh
@@ -5,4 +5,6 @@ docker rm vault
 
 sudo rm -rvf /srv/vault/file/raft/*
 sudo rm -rvf /srv/vault/config/.vault_*
+sudo rm -rvf /srv/vault/policies/*
+sudo rm -rvf /srv/vault/config/policies/*
 sudo rm /srv/vault/ansible_done*
