2e3f0c690dc2f1a7d7416dc9a390dcf71823a35c
Lars With <lars@with.de>
2022-01-16 11:48:51 +0100

    add cleanups

--- try/cleanup.sh
+++ try/cleanup.sh
@@ -3,6 +3,10 @@
 docker kill vault
 docker rm vault
 
+sudo killall -e vault
+
 sudo rm -rvf /srv/vault/*
+sudo rm -rvf /srv/vault/.ansible*
+sudo rmdir /srv/vault
 
 export VAULT_ADDR=http://127.0.0.1:9200
